2dd8f15a7daec7e992907db3800e89ed0a5ae087
Allon Nam <allon.nam96@gmail.com>
2023-11-02 16:55:38 -0400

    jbuiler

--- pokedex-rails-backend/app/controllers/api/pokemon_controller.rb
+++ pokedex-rails-backend/app/controllers/api/pokemon_controller.rb
@@ -2,4 +2,22 @@ class Api::PokemonController < ApplicationController
     def types
         render json: Pokemon::TYPES 
     end
+
+    def index
+        @pokemons = Pokemon.all
+        render :index
+    end
+
+    def show
+        @pokemon = Pokemon.find(params[:id])
+
+        if @pokemon
+            render :show
+        else
+            render json: @pokemon.errors.full_messages, status: 404
+        end
+
+    end
+
+    
 end
